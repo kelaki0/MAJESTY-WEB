fb22830cc28f2b31035be972cd5f1567d59971ab
langat <zombie@kali>
2024-11-21 12:27:51 +0300

    collections

--- header.php
+++ header.php
@@ -38,13 +38,13 @@
         <div class="nav__menu" id="nav__menu" role="menu">
             <ul class="nav__list">
                 <!-- Home Link -->
-                <li><a href="#" class="nav__link" role="menuitem">Home</a></li>
+                <li><a href="index.php" class="nav__link" role="menuitem">Home</a></li>
 
                 <!--=============== DROPDOWN: COLLECTIONS ===============-->
                 <li class="dropdown__item">
-                    <button class="nav__link" aria-haspopup="true" aria-expanded="false">
-                        Collections 
-                    </button>
+                    <a href="collections.php" class="nav__link" role="menuitem" aria-haspopup="true" aria-expanded="false">
+                        Collections
+                    </a>
                     <ul class="dropdown__menu" role="menu">
                         <!-- Submenu 1: Electrical Products -->
                         <li class="dropdown__subitem">
@@ -60,9 +60,59 @@
                         <li class="dropdown__subitem">
                             <a href="indoor_lighting.php" class="dropdown__link" role="menuitem">Indoor Lighting</a>
                             <ul class="dropdown__submenu" role="menu">
-                                <li><a href="#" class="dropdown__sublink" role="menuitem">Conduit Pipes</a></li>
-                                <li><a href="#" class="dropdown__sublink" role="menuitem"><i class="ri-cash-line"></i> Payments</a></li>
-                                <li><a href="#" class="dropdown__sublink" role="menuitem"><i class="ri-refund-2-line"></i> Refunds</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Recessed Fixtures</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> Track Lighting</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> Pendants</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Wall Sconces</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> Ceiling Lighting</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> Ambient Ligting</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Task Lighting</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Submenu 3: Outdoor Lighting -->
+                        <li class="dropdown__subitem">
+                            <a href="indoor_lighting.php" class="dropdown__link" role="menuitem">Outdoor Lighting</a>
+                            <ul class="dropdown__submenu" role="menu">
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Spotlight</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Floodlights</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> Up/Down ligths</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Step Lights</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Garden Lights</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> Bollard Lights</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem"> String Lights</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Submenu 4: Security Systems -->
+                        <li class="dropdown__subitem">
+                            <a href="indoor_lighting.php" class="dropdown__link" role="menuitem">Security Systems</a>
+                            <ul class="dropdown__submenu" role="menu">
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">CCTV </a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Gates</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Access Controls</a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Submenu 5: Solar Systems -->
+                        <li class="dropdown__subitem">
+                            <a href="indoor_lighting.php" class="dropdown__link" role="menuitem">Solar Systems</a>
+                            <ul class="dropdown__submenu" role="menu">
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Off Grid Solar Systems </a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Backup Solar Power</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Solar4America </a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">ZNshine Solars </a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Qcells </a></li>
+                            </ul>
+                        </li>
+
+                        <!-- Submenu 6: Heavy Machinery -->
+                        <li class="dropdown__subitem">
+                            <a href="indoor_lighting.php" class="dropdown__link" role="menuitem">Heavy Machinery</a>
+                            <ul class="dropdown__submenu" role="menu">
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Drilling</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Generators</a></li>
+                                <li><a href="#" class="dropdown__sublink" role="menuitem">Welding</a></li>
                             </ul>
                         </li>
                     </ul>
@@ -70,35 +120,36 @@
 
                 <!--=============== DROPDOWN: HELP ===============-->
                 <li class="dropdown__item">
-                    <button class="nav__link"  aria-expanded="false">
+                    <a href="help.php" class="nav__link" role="menuitem" aria-expanded="false">
                         Help
-                    </button>
+                    </a>
                     <ul class="dropdown__menu" role="menu">
-                        <li><a href="#" class="dropdown__link" role="menuitem"><i class="ri-user-line"></i> FAQ</a></li>
-                        <li><a href="#" class="dropdown__link" role="menuitem"><i class="ri-lock-line"></i> Support</a></li>
-                        <li><a href="#" class="dropdown__link" role="menuitem"><i class="ri-message-3-line"></i> Contact Us</a></li>
+                        <li><a href="faq.php" class="dropdown__link" role="menuitem"><i class="ri-user-line"></i> FAQ</a></li>
+                        <li><a href="support.php" class="dropdown__link" role="menuitem"><i class="ri-lock-line"></i> Support</a></li>
+                        <li><a href="contact_us.php" class="dropdown__link" role="menuitem"><i class="ri-message-3-line"></i> Contact Us</a></li>
                     </ul>
                 </li>
 
                 <!--=============== CART LINK ===============-->
-                <li><a href="#" class="nav__link" role="menuitem"><i class="ri-shopping-cart-2-fill"></i> Cart</a></li>
+                <li><a href="cart.php" class="nav__link" role="menuitem"><i class="ri-shopping-cart-2-fill"></i> Cart</a></li>
 
                 <!--=============== DROPDOWN: ACCOUNT ===============-->
                 <li class="dropdown__item">
-                    <button class="nav__link" aria-haspopup="true" aria-expanded="false">
+                    <a href="account.php" class="nav__link" role="menuitem" aria-haspopup="true" aria-expanded="false">
                         <i class="ri-account-pin-circle-fill"></i> Account
-                    </button>
+                    </a>
                     <ul class="dropdown__menu" role="menu">
                         <li><a href="signin.php" class="dropdown__link" role="menuitem"><i class="ri-user-line"></i> Signin</a></li>
-                        <li><a href="#" class="dropdown__link" role="menuitem"><i class="ri-lock-line"></i> My Account</a></li>
-                        <li><a href="#" class="dropdown__link" role="menuitem"><i class="ri-message-3-line"></i> Orders</a></li>
-                        <li><a href="#" class="dropdown__link" role="menuitem"><i class="ri-message-3-line"></i> Saved Items</a></li>
+                        <li><a href="my_account.php" class="dropdown__link" role="menuitem"><i class="ri-lock-line"></i> My Account</a></li>
+                        <li><a href="orders.php" class="dropdown__link" role="menuitem"><i class="ri-message-3-line"></i> Orders</a></li>
+                        <li><a href="saved_items.php" class="dropdown__link" role="menuitem"><i class="ri-message-3-line"></i> Saved Items</a></li>
                     </ul>
                 </li>
             </ul>
         </div>
     </nav>
 </header>
+
 
 
     <div class="search-social-container">
